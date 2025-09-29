9702f0a980521d19b26946db03cd115acd3d17ee
Ota Fejfar <ota.fejfar@gmail.com>
2025-09-29 16:47:29 +0200

    refactor: kbdapi_key_t col and row now grouped as coord (matrix coordinates). Added coordination validity flag is_valid
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

--- core/kbd_types.h
+++ core/kbd_types.h
@@ -94,8 +94,12 @@ typedef struct
     kbdapi_key_type_t type;
 
     /* Position */
-    uint16_t col;
-    uint16_t row;
+    struct
+    {
+        bool_t is_valid;
+        uint16_t col;
+        uint16_t row;
+    } coord;                    /* The key coordinates within the key matrix */
 
     /* Flags */
     bool_t is_pressed : 1;      /* True if the key is actually pressed */
