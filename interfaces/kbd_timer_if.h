46711a116bb99c756e54e6222647625e566a950a
Ota Fejfar <ota.fejfar@gmail.com>
2026-03-20 17:39:16 +0100

    feat: kbd_timer_if - Adding innterface for accessing the system timer
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

--- interfaces/kbd_timer_if.h
+++ interfaces/kbd_timer_if.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (C) 2025  Dygma Lab S.L.
+ * Copyright (C) 2026  Dygma Lab S.L.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,32 +22,40 @@
  * SOFTWARE.
  */
 
-#ifndef __KEYBOARD_API_H_
-#define __KEYBOARD_API_H_
+#ifndef __KBD_TIMER_IF_H_
+#define __KBD_TIMER_IF_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include "kbd_core.h"
-#include "kbd_timer_if.h"
+
+/*****************************************************/
+/*                KBD timer functions                */
+/*****************************************************/
+typedef uint64_t kbdtimer_t;
+
+typedef void (* kbdtimer_set_ms_fn)( kbdtimer_t * p_timer, uint32_t ms );
+typedef bool (* kbdtimer_check_fn)( kbdtimer_t * p_timer );
+
+extern void kbdtimer_set_ms( kbdtimer_t * p_timer, uint32_t ms );
+extern bool kbdtimer_check( kbdtimer_t * p_timer );
+
+/*****************************************************/
+/*                KBD timer interface                */
+/*****************************************************/
 
 typedef struct
 {
-    /* Timer interface */
-    kbdtimif_config_t kbdtimif;
-} kbdapi_config_t;
+    kbdtimer_set_ms_fn set_ms_fn;
+    kbdtimer_check_fn check_fn;
+} kbdtimif_config_t;
 
-typedef uint32_t kbdapi_key_report_lock_t;
-
-extern result_t kbdapi_init( const kbdapi_config_t * p_config );
-
-extern result_t kbdapi_key_report_lock_init( kbdapi_key_report_lock_t * p_lock );
-extern result_t kbdapi_key_report_enable( kbdapi_key_report_lock_t * p_lock );
-extern result_t kbdapi_key_report_disable( kbdapi_key_report_lock_t * p_lock );
+extern result_t kbdtimif_init( const kbdtimif_config_t * p_config );
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __KEYBOARD_API_H_ */
+#endif /* __KBD_TIMER_IF_H_ */
